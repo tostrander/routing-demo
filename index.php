c591635bf63e3b3f99f7050c7940881f39a846dc
Tina Ostrander <tostrander@greenriver.edu>
2018-01-12 17:46:23 -0500

    added additional routes

--- index.php
+++ index.php
@@ -6,9 +6,24 @@ require_once('vendor/autoload.php');
 //Create an instance of the Base class
 $f3 = Base::instance();
 
+//Set debug level
+$f3->set('DEBUG', 3);
+
 //Define a default route
 $f3->route('GET /', function() {
     echo '<h1>Routing Demo</h1>';
+}
+);
+
+//Define a page1 route
+$f3->route('GET /page1', function() {
+    echo '<h1>This is page 1</h1>';
+}
+);
+
+//Define a page1 route
+$f3->route('GET /page1/subpage-a', function() {
+    echo '<h1>This is page 1, Subpage A</h1>';
 }
 );
 
