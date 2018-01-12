6a619e66d86c93e815772c1195dfb0983538326c
Tina Ostrander <tostrander@greenriver.edu>
2018-01-12 14:16:40 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    echo '<h1>Routing Demo</h1>';
+}
+);
+
+//Run fat free
+$f3->run();
