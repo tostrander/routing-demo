5947edec1bd8d76cf21b30f734755cc5697c4d6d
Tina Ostrander <tostrander@greenriver.edu>
2018-01-12 17:18:22 -0500

    First commit

--- index.php
+++ index.php
@@ -6,24 +6,9 @@ require_once('vendor/autoload.php');
 //Create an instance of the Base class
 $f3 = Base::instance();
 
-//Set debug level
-$f3->set('DEBUG', 3);
-
 //Define a default route
 $f3->route('GET /', function() {
     echo '<h1>Routing Demo</h1>';
-}
-);
-
-//Define a page1 route
-$f3->route('GET /page1', function() {
-    echo '<h1>This is page 1</h1>';
-}
-);
-
-//Define a page1 route
-$f3->route('GET /page1/subpage-a', function() {
-    echo '<h1>This is page 1, subpage a</h1>';
 }
 );
 
