df3baa2fe85d1a5cd1f6a13f529c1c934b87c6d9
Tina Ostrander <tostrander@greenriver.edu>
2018-01-16 16:18:05 -0500

    added toe-ring route

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-session_start();
-
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
@@ -17,6 +15,26 @@ $f3->set('DEBUG', 3);
 //Define a default route
 $f3->route('GET /', function() {
     echo '<h1>Routing Demo</h1>';
+}
+);
+
+//Define a page1 route
+$f3->route('GET /page1', function() {
+    echo '<h1>This is page 1</h1>';
+}
+);
+
+//Define a page1 route
+$f3->route('GET /page1/subpage-a', function() {
+    echo '<h1>This is page 1, Subpage A</h1>';
+}
+);
+
+//Define a jewelry route
+$f3->route('GET /jewelry/rings/toe-rings', function() {
+    //echo '<h1>Buy a toe ring today!</h1>';
+    $template = new Template();
+    echo $template->render('views/toe-rings.html');
 }
 );
 
@@ -75,27 +93,7 @@ $f3->route('GET /language/@lang',
     }
 );
 
-//Define a default route
-$f3->route('GET /jewelry/rings/toe-rings', function() {
-    //echo '<h1>Buy a toe ring today!</h1>';
-    $template = new Template();
-    echo $template->render('views/toe-rings.html');
-}
-);
 
-/*
-//Define a page1 route
-$f3->route('GET /page1', function() {
-    echo '<h1>This is page 1</h1>';
-}
-);
-
-//Define a page1 route
-$f3->route('GET /page1/subpage-a', function() {
-    echo '<h1>This is page 1, Subpage A</h1>';
-}
-);
-*/
 
 
 //Run fat free
