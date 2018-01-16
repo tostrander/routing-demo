9ed2170c468d46a4c1ec1b55461e1acc40227b5e
Tina Ostrander <tostrander@greenriver.edu>
2018-01-16 13:42:20 -0500

    Added language route

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 //Require the autoload file
 require_once('vendor/autoload.php');
 
@@ -15,6 +18,41 @@ $f3->route('GET /', function() {
 }
 );
 
+//Define a route using parameters
+$f3->route('GET /hello/@name',
+    function($f3, $params) {
+    $name = $params['name'];
+    echo "<h1>Hello, $name</h1>";
+}
+);
+
+//Define a route using parameters
+$f3->route('GET /language/@lang',
+    function($f3, $params) {
+        switch($params['lang']){
+            case 'swahili':
+                echo 'Jumbo!'; break;
+            case 'spanish':
+                echo 'Hola!'; break;
+            case 'russian':
+                echo 'Privet!'; break;
+            case 'farsi':
+                echo 'Salam!'; break;
+            default:
+                echo 'Hello!';
+        }
+    }
+);
+
+//Define a default route
+$f3->route('GET /jewelry/rings/toe-rings', function() {
+    //echo '<h1>Buy a toe ring today!</h1>';
+    $template = new Template();
+    echo $template->render('views/toe-rings.html');
+}
+);
+
+/*
 //Define a page1 route
 $f3->route('GET /page1', function() {
     echo '<h1>This is page 1</h1>';
@@ -26,6 +64,8 @@ $f3->route('GET /page1/subpage-a', function() {
     echo '<h1>This is page 1, Subpage A</h1>';
 }
 );
+*/
+
 
 //Run fat free
 $f3->run();
